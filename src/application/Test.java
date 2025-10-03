dfa59e6a5cfd2c3571501492f7ad2acb99866beb
riadh <riadh2002belg1cem@gmail.com>
2025-10-03 17:18:29 +0200

    patch

--- src/application/Test.java
+++ src/application/Test.java
@@ -4,6 +4,6 @@ public class Test {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         //commentaire wouuuh 
-        System.out.println("je suis le master");
+        System.out.println("je suis le master riadh was here");
     }
 }
