27ecfaab5a226998e58decb3084eca586f06372a
AYOOZ <ayaboussaid2022@gmail.com>
2025-10-03 11:15:20 +0100

    Initial commit

--- src/application/Test.java
+++ src/application/Test.java
@@ -3,6 +3,6 @@ package application;
 public class Test {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        System.out.println("Hello");
+        System.out.println("Hellooooooooooooooo");
     }
 }
