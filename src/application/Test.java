999e522cfb7515429debaa17d8974096ecc3ed4e
AYOOZ <ayaboussaid2022@gmail.com>
2025-10-03 11:49:21 +0100

    dev commit

--- src/application/Test.java
+++ src/application/Test.java
@@ -4,6 +4,6 @@ public class Test {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         //commentaire
-        System.out.println("Hellooooooooooooooo");
+        System.out.println("je suis dev");
     }
 }
